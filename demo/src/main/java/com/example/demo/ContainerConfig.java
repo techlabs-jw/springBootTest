406ec325c69d09cc74e52fff361750a35bb60c5f
hanjaewon <jaewonhan@amanda.co.kr>
2022-04-12 17:39:24 +0900

    tt

---
+++ demo/src/main/java/com/example/demo/ContainerConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ContainerConfig {
+    @Value("${tomcat.ajp.protocol}")
+    String ajpProtocol;
+
+    @Value("${tomcat.ajp.port}")
+    int ajpPort;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createAjpConnector());
+        return tomcat;
+    }
+
+    private Connector createAjpConnector() {
+        Connector ajpConnection = new Connector(ajpProtocol);
+        ajpConnection.setPort(ajpPort);
+        ajpConnection.setSecure(false);
+        ajpConnection.setAllowTrace(false);
+        ajpConnection.setScheme("http");
+        ((AbstractAjpProtocol<?>)ajpConnection.getProtocolHandler()).setSecretRequired(false);
+        return ajpConnection;
+    }
+
+
+
+}
